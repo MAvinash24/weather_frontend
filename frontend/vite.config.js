88cbfaccf009d3cf3ff4490e5bee676a4c1687eb
M_Avinash_ <mavinash0320@gmail.com>
2025-11-26 17:57:23 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://weather-backend-wjxw.onrender.com",
         changeOrigin: true,
         secure: false
       }
